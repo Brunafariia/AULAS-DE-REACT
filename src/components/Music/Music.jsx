3451c013826600e2a2b54de774a454176c8c6f33
Brunafariia <brunafaria.souza@gmail.com>
2023-03-14 23:11:31 -0300

    implement music component

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -1,8 +1,10 @@
-export function Cardas () {
+import "./Music.css";
+export function Music({ image, title, author }) {
     return (
-        <div className="card">
-
+        <div className="music">
+            <img src={image} />
+            <h3>{title}</h3>
+            <span>{author}</span>
         </div>
-    )
-
+    );
 }
