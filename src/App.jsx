a9aa6acf208e6fb57448f83b1689754674aecfe8
Brunafariia <brunafaria.souza@gmail.com>
2023-03-20 14:08:29 -0300

    Desafio 3- cadastro de pessoas

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@
 // import { QuestaoQuiz } from "./components/QuestaoQuiz/QuestaoQuiz";
 // import { Sorteio } from "./components/Sorteio/Sorteio";
 // import { Desafio1 } from "./components/Desafio1/Desafio1";
-import { Desafio2 } from "./components/Desafio2/Desafio2";
+// import { Desafio2 } from "./components/Desafio2/Desafio2";
+import { Desafio3 } from "./components/Desafio3/Desafio3";
 export function App() {
     return (
         <>
@@ -32,7 +33,8 @@ export function App() {
             {/* <Abas /> */}
             {/* <QuestaoQuiz /> */}
             {/* <Desafio1 /> */}
-            <Desafio2 /> 
+            {/* <Desafio2 />  */}
+            <Desafio3 />
             
         </>
     );
